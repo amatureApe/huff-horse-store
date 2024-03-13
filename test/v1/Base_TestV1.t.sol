6de2c12fbd793f548a403152c391ee16b329e171
amatureApe <amatureApe@gmail.com>
2024-03-13 19:56:31 +0800

    add testing

---
+++ test/v1/Base_TestV1.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GLP-3.0-only
+pragma solidity 0.8.20;
+
+import {HorseStore} from "../../src/horseStoreV1/HorseStore.sol";
+import {Test, console2} from "forge-std/test.sol";
+
+abstract contract Base_TestV1 is Test {
+    HorseStore public horseStore;
+
+    function setUp() public virtual {
+        horseStore = new HorseStore();
+    }
+
+    function testReadValue() public {
+        uint256 initialValue = horseStore.readNumberOfHorses();
+        assertEq(initialValue, 0);
+    }
+}
---
+++ test/v1/HorseStoreHuff.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GLP-3.0-only
+pragma solidity 0.8.20;
+
+import {Base_TestV1} from "./Base_TestV1.t.sol";
+
+contract HorseStoreHuff is Base_TestV1 {
+    function setUp() public override {}
+}
