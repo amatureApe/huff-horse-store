a1fc540fe315837dc8c9af73ffededd2520fb9e2
amatureApe <amatureApe@gmail.com>
2024-03-13 20:16:12 +0800

    add more testing

--- test/v1/HorseStoreHuff.t.sol
+++ test/v1/HorseStoreHuff.t.sol
@@ -1,8 +1,15 @@
 // SPDX-License-Identifier: GLP-3.0-only
 pragma solidity 0.8.20;
 
-import {Base_TestV1} from "./Base_TestV1.t.sol";
+import {Base_TestV1, HorseStore} from "./Base_TestV1.t.sol";
+import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
 
 contract HorseStoreHuff is Base_TestV1 {
-    function setUp() public override {}
+    string public constant HORSE_STORE_HUFF_LOCATION =
+        "horseStoreV1/HorseStore";
+    function setUp() public override {
+        horseStore = HorseStore(
+            HuffDeployer.config().deploy(HORSE_STORE_HUFF_LOCATION)
+        );
+    }
 }
